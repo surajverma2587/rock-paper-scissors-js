76e935da303bac465004937a4be816156ba89619
gingus55 <gingus55@hotmail.com>
2021-09-25 12:55:49 +0100

    functions in

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
 console.log("Hi everyone");
+
+// should return r s p or R S P??
+const getUserValue = function(){
+
+    return
+}
+
+// validate input true/false
+const validateValue = function(){
+
+    return
+}
+
+// get computer value r s p
+const getComputerValue = function(){
+    return
+}
+
+// game logic win lose draw
+const getGameResult = function(){
+    return
+}
+
+//  calculate and log result
+const displayScore = function(){
+    return
+}
